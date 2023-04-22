4d255d95048a2241e9f84526152eab16faf45a60
varunreddy95 <varunreddy_95@outlook.com>
2023-04-22 23:37:26 +0200

    Configuration Properties

---
+++ learn-spring-boot/src/main/java/com/varunreddy95/springboot/learnspringboot/CurrencyServiceConfiguration.java
@@ -0,0 +1,37 @@
+package com.varunreddy95.springboot.learnspringboot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix="currecy-services")
+@Component
+public class CurrencyServiceConfiguration {
+
+	private String url;
+	private String username;
+	private String key;
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	public String getKey() {
+		return key;
+	}
+}

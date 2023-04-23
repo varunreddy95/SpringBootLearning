8ca59ee827548b24f9b4ff5c79ddaddcaeb01710
varunreddy95 <varunreddy_95@outlook.com>
2023-04-23 13:56:51 +0200

    Started with JPA, Hibernate, Springboot Data JPA, Springboot JDBC

---
+++ learn-jpa-hibernate/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table formula1
+{
+	id bigint unique not null,
+	teamname  varchar(255) not nul;,
+	drivername varchar(255) not null,
+	primary key (id)
+};
---
+++ learn-jpa-hibernate/src/test/java/com/varunreddy95/springboot/learnjpahibernate/LearnJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varunreddy95.springboot.learnjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
